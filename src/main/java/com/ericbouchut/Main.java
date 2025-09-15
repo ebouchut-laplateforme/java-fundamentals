92840c2adf4f9a31f09c3c951506415986d53ef5
Eric Bouchut <ebouchut@gmail.com>
2025-09-15 09:38:25 +0200

    refactor(Main): Empty the Main class
    Will use a main method instead, in each key class for testing

--- src/main/java/com/ericbouchut/Main.java
+++ src/main/java/com/ericbouchut/Main.java
@@ -7,20 +7,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        for (int i= 1; i <= 1_000_000; i++)  {
-            System.out.println("Poney " + i);
-        }
-//        Calculator calculator = new Calculator();
-//
-//        Scanner scanner = new Scanner(System.in);
-//        while (scanner.hasNext()) {
-//            int a = scanner.nextInt();
-//            String operation = scanner.next();
-//            int b = scanner.nextInt();
-//
-//
-//        }
+        // TODO: Test sandbox
     }
-
 }
