90b6b5e2b0efacfa69bc6c5741841f3cf8eb0afa
Eric Bouchut <ebouchut@gmail.com>
2025-09-15 15:46:35 +0200

    feat(array): Sort an array using the bubble sort method
    Cf. exercise 49
    
    See: https://www.udemy.com/course/java-fundamentals-beginners/learn/lecture/46065461#overview

---
+++ src/main/java/com/ericbouchut/type/array/ArrayBubbleSort.java
@@ -0,0 +1,60 @@
+package com.ericbouchut.type.array;
+
+/**
+ * Ex 49:
+ * Sort an array using the <b>bubble sort</b> method.
+ * O(N<sup>2</sup>) (Quadratic complexity_).
+ *
+ * @see <a href="https://www.udemy.com/course/java-fundamentals-beginners/learn/lecture/46065461#overview">Java Fundamentals Course on Udemy</a>
+ */
+public class ArrayBubbleSort {
+
+    /**
+     * Sort the passed in integer array using the bubble sort method.
+     * This method modifies the passed in array.
+     *
+     * @param array an int array
+     */
+    public void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        int buffer;
+
+        for (int i=0; i < array.length; i++) {
+
+            for (int j=0; j < array.length && j+1 < array.length; j++) {
+                isSorted = array[j] <= array[j+1];
+
+                if (!isSorted) {
+                    // Swap the 2 consecutive array items to sort them in ascending order
+                    buffer     = array[j];
+                    array[j]   = array[j+1];
+                    array[j+1] = buffer;
+                }
+            }
+        }
+        System.out.println(isSorted);
+    }
+
+    /**
+     * Display the passed-in array on the standard output.
+     *
+     * @param array an integer array
+     */
+    public void display(int[] array) {
+        for(int item: array) {
+            System.out.printf("%d ", item);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayBubbleSort ex49 = new ArrayBubbleSort();
+
+        // Build an integer array filled with random values [1..10]
+        int[] numbers = RandomValuesArrayBuilder.build(10, 10);
+        ex49.display(numbers);
+
+        ex49.bubbleSort(numbers);
+        ex49.display(numbers);
+    }
+}
