feef5add684233b6fb569a638f0032dda75ea76a
Eric Bouchut <ebouchut@gmail.com>
2025-10-06 11:29:38 +0200

    refactor(MotionSickness): Add a TODO to make ISymptomProcessor less BufferedReader-centric
    so that we can use it with whatever we can read from such as an URL for instance

--- src/main/java/com/ericbouchut/io/motionsickness/ISymptomProcessor.java
+++ src/main/java/com/ericbouchut/io/motionsickness/ISymptomProcessor.java
@@ -4,5 +4,6 @@ import java.io.BufferedReader;
 import java.util.Map;
 
 public interface ISymptomProcessor {
+    // TODO: Refactor to make the processor less BufferedReader centric, to be able to read from something else such as an URL.
     Map<String, Integer> process(BufferedReader reader) throws SymptomException;
 }
